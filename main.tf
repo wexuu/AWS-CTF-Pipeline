7e2cfe292eb14fa26cbec69fe23a47f2fb4385a1
wexuuu <47903842+wexuu@users.noreply.github.com>
2025-12-19 20:55:59 +0100

    ffe

--- main.tf
+++ main.tf
@@ -14,22 +14,10 @@ variable "aws_region" {
   default = "eu-central-1"
 }
 
-# ZMIEŃ TO na unikalną nazwę
-variable "bucket_name" {
-  type    = string
-  default = "cloud-ctf-change-me-123456"
-}
 
 provider "aws" {
   region = var.aws_region
 }
-
-resource "aws_s3_bucket" "lab" {
-  bucket        = var.bucket_name
-  force_destroy = true
-
-  tags = {
-    Project = "cloud-ctf"
-    Env     = "sandbox"
-  }
+resource "aws_s3_bucket" "tf-state" {
+  bucket        = "cloud-ctf-tf-state"
 }
