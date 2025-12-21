96ec01ff4e8272c3641f635f391883736d3fc9de
wexuuu <47903842+wexuu@users.noreply.github.com>
2025-12-21 13:56:10 +0100

    Added SSH keys for website access

--- public-ec2.tf
+++ public-ec2.tf
@@ -13,7 +13,7 @@ resource "aws_instance" "web" {
   instance_type          = "t3.micro"
   subnet_id              = aws_subnet.public.id
   vpc_security_group_ids = [aws_security_group.public_web_sg.id]
-  key_name               = aws_key_pair.cloud_ctf-ec2-kuba-pubkey
+  key_name               = aws_key_pair.cloud_ctf-ec2-kuba-pubkey.key_name
   user_data              = <<-EOF
     #!/bin/bash
     yum update -y
