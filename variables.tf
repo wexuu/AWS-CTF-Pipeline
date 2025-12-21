2a24d891d25d83f7621fe52f97bf6cd3beeb8312
wexuuu <47903842+wexuu@users.noreply.github.com>
2025-12-21 13:40:59 +0100

    Added SSH keys for website access

--- variables.tf
+++ variables.tf
@@ -5,5 +5,5 @@ variable "my_ip_cidr" {
 
 variable "cloud-ctf-ec2-kuba-ssh-public-path" {
   type    = string
-  default = "./keys/cloud-ctf-ec2-kuba.pub"
+  default = "/keys/cloud-ctf-ec2-kuba.pub"
 }
