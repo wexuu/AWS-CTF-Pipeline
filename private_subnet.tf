9fe771c460439cb5929db1b78a15188c308e54c1
wexuuu <47903842+wexuu@users.noreply.github.com>
2025-12-22 11:49:59 +0100

    Private end

--- private_subnet.tf
+++ private_subnet.tf
@@ -1,6 +1,13 @@
+resource "aws_route_table" "public_route" {
+  vpc_id = aws_vpc.vpc-main.id
+  tags = {
+    Name    = "cloud-ctf-private-route"
+    Project = "cloud-ctf"
+  }
+}
 resource "aws_subnet" "private" {
-  vpc_id     = aws_vpc.vpc-main.id
-  cidr_block = "10.0.0.128/28"
+  vpc_id                  = aws_vpc.vpc-main.id
+  cidr_block              = "10.0.0.128/28"
   tags = {
     Name    = "cloud-ctf-subnet-private"
     Project = "cloud-ctf"
