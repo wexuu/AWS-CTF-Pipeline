2f86c1a5ed76604104d139f63611d3f8344fdb08
wexuuu <47903842+wexuu@users.noreply.github.com>
2025-12-22 13:05:13 +0100

    JSHOP added

--- public-end.tf
+++ public-end.tf
@@ -61,7 +61,7 @@ resource "aws_security_group" "public_web_sg" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = [var.my_ip_cidr]
+    cidr_blocks = ["0.0.0.0/0"]
   }
   egress {
     from_port   = 0
