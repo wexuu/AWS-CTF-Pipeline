3690b82770b8d33fe4096a7b29c9f69ec82ab949
wexuuu <47903842+wexuu@users.noreply.github.com>
2025-12-22 13:16:12 +0100

    nat

--- private_subnet.tf
+++ private_subnet.tf
@@ -13,3 +13,34 @@ resource "aws_subnet" "private" {
     Project = "cloud-ctf"
   }
 }
+resource "aws_route_table" "private_route" {
+  vpc_id = aws_vpc.lab.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat.id
+  }
+
+  tags = {
+    Name = "cloud-ctf-private-rt"
+    Project = "cloud-ctf"
+  }
+}
+
+resource "aws_route_table_association" "private_assoc" {
+  subnet_id      = aws_subnet.private.id
+  route_table_id = aws_route_table.private_route.id
+}
+resource "aws_eip" "nat_eip" {
+  
+}
+
+resource "aws_nat_gateway" "nat" {
+  allocation_id = aws_eip.nat_eip.id
+  subnet_id     = aws_subnet.public.id
+
+  tags = {
+    Name = "cloud-ctf-nat-gateway"
+    Project = "cloud-ctf"
+  }
+}
