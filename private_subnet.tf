d511e095068ad2d7dd3225467d2a1f15f1b49be3
wexuuu <47903842+wexuu@users.noreply.github.com>
2025-12-22 13:17:26 +0100

    nat

--- private_subnet.tf
+++ private_subnet.tf
@@ -1,10 +1,3 @@
-resource "aws_route_table" "private_route" {
-  vpc_id = aws_vpc.vpc-main.id
-  tags = {
-    Name    = "cloud-ctf-private-route"
-    Project = "cloud-ctf"
-  }
-}
 resource "aws_subnet" "private" {
   vpc_id     = aws_vpc.vpc-main.id
   cidr_block = "10.0.0.128/28"
